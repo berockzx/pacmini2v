d35780f3d82f3faf9b63bbd66722d5afd0dc4ae2
berockzx <127618259+berockzx@users.noreply.github.com>
2025-11-04 00:08:31 +0100

    Delete v1 directory

--- v1/js/spixi-shim.js
+++
@@ -1,6 +0,0 @@
-window.Spixi = window.Spixi || {
-  storage: {
-    get: async (k) => JSON.parse(localStorage.getItem('spx:'+k)||'null'),
-    set: async (k, v) => localStorage.setItem('spx:'+k, JSON.stringify(v))
-  }
-};
